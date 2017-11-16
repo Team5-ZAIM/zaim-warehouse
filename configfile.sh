7b2e505ab8698d5a6b1e944bd667233281ab1365
blowjc <blowjc@clarkson.edu>
2017-11-16 12:33:12 -0500

    Uses a variable to store the config.json filename

--- configfile.sh
+++ configfile.sh
@@ -1,17 +1,18 @@
-# Create file named config.json in project directory and insert MySQL Connection Credentials
-echo "Creating config.json file"
-printf "{\n" > config.json
+# Create file named $CONFIGFILE in project directory and insert MySQL Connection Credentials
+CONFIGFILE=config.json # Filename of config file
+echo "Creating $CONFIGFILE file"
+printf "{\n" > $CONFIGFILE
 printf "Hostname: "
 read HNINPUT
-printf "\t\"host\": \"$HNINPUT\"\n" >> config.json
+printf "\t\"host\": \"$HNINPUT\"\n" >> $CONFIGFILE
 printf "Username: "
 read UNINPUT
-printf "\t\"user\": \"$UNINPUT\"\n"  >> config.json
+printf "\t\"user\": \"$UNINPUT\"\n"  >> $CONFIGFILE
 printf "Password: "
 read -s PWINPUT
-printf "\t\"password\": \"$PWINPUT\"\n" >> config.json
+printf "\t\"password\": \"$PWINPUT\"\n" >> $CONFIGFILE
 printf "\n"
 printf "Database: "
 read DBINPUT
-printf "\t\"database\": \"$DBINPUT\"\n" >> config.json
-printf "}\n" >> config.json
+printf "\t\"database\": \"$DBINPUT\"\n" >> $CONFIGFILE
+printf "}\n" >> $CONFIGFILE
