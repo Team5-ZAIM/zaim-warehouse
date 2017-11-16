56b23c9f87cda424da7b2e5e31c948476fd3f081
blowjc <blowjc@clarkson.edu>
2017-11-16 12:20:27 -0500

    Writes to config.json instead of package.json
    Also hides passwords while typing them.

--- configfile.sh
+++ configfile.sh
@@ -1,6 +1,6 @@
 # Create file named config.json in project directory and insert MySQL Connection Credentials
 echo "Creating package.json file"
-printf "{\n" > config.json
+printf "{\n" > package.json
 printf "Hostname: "
 read HNINPUT
 printf "\t\"host\": \"$HNINPUT\"\n" >> config.json
@@ -8,8 +8,9 @@ printf "Username: "
 read UNINPUT
 printf "\t\"user\": \"$UNINPUT\"\n"  >> config.json
 printf "Password: "
-read PWINPUT
+read -s PWINPUT
 printf "\t\"password\": \"$PWINPUT\"\n" >> config.json
+printf "\n"
 printf "Database: "
 read DBINPUT
 printf "\t\"database\": \"$DBINPUT\"\n" >> config.json
